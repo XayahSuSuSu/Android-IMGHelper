f0e84eb235e94c50ce30673d470dc9eb78bcfddb
XayahSuSuSu <1249475336@qq.com>
2021-04-24 23:41:52 +0800

    Update version to 1.0.0
    *Use another way to flash images
    *Support dtbo flashing

--- app/src/main/java/com/xayah/imghelper/Activity/IMGExtractActivity.kt
+++ app/src/main/java/com/xayah/imghelper/Activity/IMGExtractActivity.kt
@@ -24,47 +24,47 @@ class IMGExtractActivity : AppCompatActivity() {
         val imgextract_cardView_boot: CardView = findViewById(R.id.imgextract_cardView_boot)
         imgextract_cardView_boot.setOnClickListener {
             imgExtract.boot("$outPath/extract")
-            Toast.makeText(this, "提取成功！存放于:$outPath/extract", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "提取成功！存放于:\n$outPath/extract/boot.img", Toast.LENGTH_LONG).show()
             val builder = AlertDialog.Builder(this)
                 .setTitle("提示")
-                .setMessage("提取成功！镜像存放于:$outPath/extract")
+                .setMessage("提取成功！镜像存放于:\n$outPath/extract/boot.img")
                 .setCancelable(true)
                 .setPositiveButton("好的") { _: DialogInterface?, which: Int -> }
                 .create()
             builder.show()
-            builder.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(Color.RED);
+            builder.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(Color.RED)
             builder.getButton(DialogInterface.BUTTON_NEGATIVE)
-                .setTextColor(Color.BLUE);
+                .setTextColor(Color.BLUE)
         }
         val imgextract_cardView_recovery: CardView = findViewById(R.id.imgextract_cardView_recovery)
         imgextract_cardView_recovery.setOnClickListener {
             imgExtract.recovery("$outPath/extract")
-            Toast.makeText(this, "提取成功！存放于:$outPath/extract", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "提取成功！存放于:\n$outPath/extract/recovery.img", Toast.LENGTH_LONG).show()
             val builder = AlertDialog.Builder(this)
                 .setTitle("提示")
-                .setMessage("提取成功！镜像存放于:$outPath/extract")
+                .setMessage("提取成功！镜像存放于:\n$outPath/extract/recovery.img")
                 .setCancelable(true)
                 .setPositiveButton("好的") { _: DialogInterface?, which: Int -> }
                 .create()
             builder.show()
-            builder.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(Color.RED);
+            builder.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(Color.RED)
             builder.getButton(DialogInterface.BUTTON_NEGATIVE)
-                .setTextColor(Color.BLUE);
+                .setTextColor(Color.BLUE)
         }
         val imgextract_cardView_dtbo: CardView = findViewById(R.id.imgextract_cardView_dtbo)
         imgextract_cardView_dtbo.setOnClickListener {
-//            imgExtract.dtbo("$outPath/extract")
-//            Toast.makeText(this, "提取成功！存放于:$outPath/extract", Toast.LENGTH_LONG).show()
+            imgExtract.dtbo("$outPath/extract")
+            Toast.makeText(this, "提取成功！存放于:\n$outPath/extract/dtbo.img", Toast.LENGTH_LONG).show()
             val builder = AlertDialog.Builder(this)
-                .setTitle("提示")
-                .setMessage("暂不支持！")
-                .setCancelable(true)
-                .setPositiveButton("好的") { _: DialogInterface?, which: Int -> }
-                .create()
+                    .setTitle("提示")
+                    .setMessage("提取成功！镜像存放于:\n$outPath/extract/dtbo.img")
+                    .setCancelable(true)
+                    .setPositiveButton("好的") { _: DialogInterface?, which: Int -> }
+                    .create()
             builder.show()
-            builder.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(Color.RED);
+            builder.getButton(AlertDialog.BUTTON_POSITIVE).setTextColor(Color.RED)
             builder.getButton(DialogInterface.BUTTON_NEGATIVE)
-                .setTextColor(Color.BLUE);
+                    .setTextColor(Color.BLUE)
         }
     }
 
