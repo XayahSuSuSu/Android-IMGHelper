af62335078a272ad02a7382d3a508f4a46d2abbb
XayahSuSuSu <1249475336@qq.com>
2021-05-01 19:28:18 +0800

    1.1.0: Use magisk shell instead [2/4]

--- app/src/main/java/com/xayah/imghelper/Activity/DtboPatchActivity.kt
+++ app/src/main/java/com/xayah/imghelper/Activity/DtboPatchActivity.kt
@@ -12,35 +12,41 @@ import com.topjohnwu.superuser.Shell
 import com.xayah.imghelper.R
 import com.xayah.imghelper.Utils.ContentUriUtil
 import com.xayah.imghelper.Utils.DialogUtil
+import com.xayah.imghelper.Utils.PathUtil
 
 class DtboPatchActivity : AppCompatActivity() {
-    val dialogUtil = DialogUtil(this)
+    // -------------------Component-------------------
     lateinit var textInputEditText_dtboPath: TextInputEditText
+    lateinit var textInputEditText_dtboRate: TextInputEditText
+    lateinit var dtbo_patch_imageView_copyright: ImageView
+    lateinit var floatingActionButton_dtboPatch: ExtendedFloatingActionButton
+
+    // -------------------Utils-------------------
+    val dialogUtil = DialogUtil(this)
+    val pathUtil = PathUtil(this)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dtbo_patch)
+        bindView() // 绑定组件
+        setListener() // 设置监听器
+    }
 
+    private fun bindView() {
         textInputEditText_dtboPath = findViewById(R.id.textInputEditText_dtboPath)
-        val textInputEditText_dtboRate: TextInputEditText =
-            findViewById(R.id.textInputEditText_dtboRate)
+        textInputEditText_dtboRate = findViewById(R.id.textInputEditText_dtboRate)
+        dtbo_patch_imageView_copyright = findViewById(R.id.dtbo_patch_imageView_copyright)
+        floatingActionButton_dtboPatch = findViewById(R.id.floatingActionButton_dtboPatch)
+    }
+
+    private fun setListener() {
         textInputEditText_dtboPath.setOnClickListener {
+            // 调用文件管理器
             val intent = Intent(Intent.ACTION_GET_CONTENT)
             intent.type = "*/*" //无类型限制
             intent.addCategory(Intent.CATEGORY_OPENABLE)
             startActivityForResult(intent, 1)
         }
-
-        val dtbo_patch_imageView_copyright: ImageView =
-            findViewById(R.id.dtbo_patch_imageView_copyright)
-        dtbo_patch_imageView_copyright.setOnClickListener {
-            dialogUtil.createPositiveButtonDialog(
-                "修补脚本修改自酷安 @神裂火织 的dtbo-tools\n已获得作者授权",
-                "好的",
-                {})
-        }
-
-        val floatingActionButton_dtboPatch: ExtendedFloatingActionButton =
-            findViewById(R.id.floatingActionButton_dtboPatch)
         floatingActionButton_dtboPatch.setOnClickListener {
             if (textInputEditText_dtboPath.text.toString()
                     .isEmpty() || textInputEditText_dtboRate.text.toString().isEmpty()
@@ -50,28 +56,20 @@ class DtboPatchActivity : AppCompatActivity() {
                     "好的",
                     {})
             } else {
-                val mRate = textInputEditText_dtboRate.text.toString()
-//                    val mDtboPatch = mutableListOf<String>()
-//                    mDtboPatch.add("echo $mRate > Info.txt")
-                val mShellPath = this.filesDir.toString() + "/scripts/Patch.sh"
-                val mDtboDirPath = textInputEditText_dtboPath.text.toString()
-                    .substring(0, textInputEditText_dtboPath.text.toString().lastIndexOf('/'))
-                val mDtboPath = textInputEditText_dtboPath.text.toString()
-                val mShellDirPath = mShellPath.substring(0, mShellPath.lastIndexOf('/'))
-//                    mDtboPatch.add("cp $mDtboPath $mShellDirPath/dtbo.img")
-//                    mDtboPatch.add("chmod 777 $mShellPath")
-//                    mDtboPatch.add("./Patch.sh")
-//                    CommandArrayUtil.executeCommand(
-//                        mDtboPatch,
-//                        mShellDirPath,
-//                        true,
-//                        true
-//                    )
                 dialogUtil.createProgressDialog {
                     Thread {
+                        val mRate = textInputEditText_dtboRate.text.toString()
+                        val mShellPath = pathUtil.scriptsPath() + "Patch.sh"
+                        val mDtboDirPath = textInputEditText_dtboPath.text.toString()
+                            .substring(
+                                0,
+                                textInputEditText_dtboPath.text.toString().lastIndexOf('/')
+                            )
+                        val mDtboPath = textInputEditText_dtboPath.text.toString()
+                        val mShellDirPath = mShellPath.substring(0, mShellPath.lastIndexOf('/'))
                         Shell.su("cd $mShellDirPath").exec()
+                        Shell.su("echo $mRate > Info.txt").exec()
                         Shell.su("cp $mDtboPath $mShellDirPath/dtbo.img").exec()
-                        Shell.su("chmod 777 $mShellPath").exec()
                         val result = Shell.su("./Patch.sh").exec().out
                         Log.d("DtboPatchActivity", result.toString())
                         it.dismiss()
@@ -84,9 +82,13 @@ class DtboPatchActivity : AppCompatActivity() {
                         }
                     }.start()
                 }
-
             }
-
+        }
+        dtbo_patch_imageView_copyright.setOnClickListener {
+            dialogUtil.createPositiveButtonDialog(
+                "修补脚本修改自酷安 @神裂火织 的dtbo-tools\n已获得作者授权",
+                "好的",
+                {})
         }
     }
 
