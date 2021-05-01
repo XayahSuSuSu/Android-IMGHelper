3ee6da6c68c5fdf16a3ef1e97cf3bd68fc36e6de
XayahSuSuSu <1249475336@qq.com>
2021-05-01 19:44:20 +0800

    1.1.0: Use magisk shell instead [3/4]

--- app/src/main/java/com/xayah/imghelper/Tools/IMGExtract.kt
+++ app/src/main/java/com/xayah/imghelper/Tools/IMGExtract.kt
@@ -1,92 +1,34 @@
 package com.xayah.imghelper.Tools
 
 import android.util.Log
+import com.topjohnwu.superuser.Shell
 import com.xayah.imghelper.Utils.CommandUtil
 
-class IMGExtract(var cachePath: String) {
-//    init {
-//        CommandUtil.executeCommand(
-//                "cp /vendor/etc/fstab.qcom $cachePath",
-//                "/dev/block/bootdevice/by-name/",
-//                true,
-//                true
-//        )
-//    }
-
-//    fun getPartition(partitionType: String): String {
-//        var partitionPath = "-1"
-//        val fstab_qcom = File(cachePath, "fstab.qcom").readText()
-//        val fstab_qcom_list = fstab_qcom.split("\n")
-//        var isUseful = false
-//        for (eachLine in fstab_qcom_list) {
-//            val partitionInfo = Regex("\\s+").split(eachLine)
-//            if (isUseful && partitionInfo.size > 1) {
-//                Log.d("IMGExtract", "分区位置:" + partitionInfo[0])
-//                Log.d("IMGExtract", "分区类型:" + partitionInfo[1])
-//                if ("/$partitionType" == partitionInfo[1]) {
-//                    partitionPath = partitionInfo[0]
-//                }
-//            }
-//            if (eachLine.contains("#<src>")) {
-//                isUseful = true
-//            }
-//        }
-//        return partitionPath
-//    }
-
-    fun boot(outPutPath: String) {
-        val IMGPath = CommandUtil.executeCommand(
-            "ls -l boot",
-            "/dev/block/by-name",
-            true,
-            true
-        )
-        Log.d("IMGExtractActivity", IMGPath)
-        val link = IMGPath.split("->")
-        val IMGlink = link[1].trim()
-        Log.d("IMGExtractActivity", IMGlink)
-        CommandUtil.executeCommand(
-            "dd if=$IMGlink of=$outPutPath/boot.img",
-            "/dev/block/by-name",
-            true,
-            true
-        )
+class IMGExtract() {
+    companion object {
+        fun boot(outPutPath: String) {
+            val IMGPath = Shell.su("ls -l /dev/block/by-name/boot").exec().out[0]
+            Log.d("IMGExtractActivity", IMGPath)
+            val link = IMGPath.split("->")
+            val IMGlink = link[1].trim()
+            Log.d("IMGExtractActivity", IMGlink)
+            Shell.su("dd if=$IMGlink of=$outPutPath/boot.img").exec()
+        }
+        fun recovery(outPutPath: String) {
+            val IMGPath = Shell.su("ls -l /dev/block/by-name/recovery").exec().out[0]
+            Log.d("IMGExtractActivity", IMGPath)
+            val link = IMGPath.split("->")
+            val IMGlink = link[1].trim()
+            Log.d("IMGExtractActivity", IMGlink)
+            Shell.su("dd if=$IMGlink of=$outPutPath/recovery.img").exec()
+        }
+        fun dtbo(outPutPath: String) {
+            val IMGPath = Shell.su("ls -l /dev/block/by-name/dtbo").exec().out[0]
+            Log.d("IMGExtractActivity", IMGPath)
+            val link = IMGPath.split("->")
+            val IMGlink = link[1].trim()
+            Log.d("IMGExtractActivity", IMGlink)
+            Shell.su("dd if=$IMGlink of=$outPutPath/dtbo.img").exec()
+        }
     }
-
-    fun recovery(outPutPath: String) {
-        val IMGPath = CommandUtil.executeCommand(
-            "ls -l recovery",
-            "/dev/block/by-name",
-            true,
-            true
-        )
-        val link = IMGPath.split("->")
-        val IMGlink = link[1].trim()
-        Log.d("IMGExtractActivity", IMGlink)
-        CommandUtil.executeCommand(
-            "dd if=$IMGlink of=$outPutPath/recovery.img",
-            "/dev/block/by-name",
-            true,
-            true
-        )
-    }
-
-    fun dtbo(outPutPath: String) {
-        val IMGPath = CommandUtil.executeCommand(
-            "ls -l dtbo",
-            "/dev/block/by-name",
-            true,
-            true
-        )
-        val link = IMGPath.split("->")
-        val IMGlink = link[1].trim()
-        Log.d("IMGExtractActivity", IMGlink)
-        CommandUtil.executeCommand(
-            "dd if=$IMGlink of=$outPutPath/dtbo.img",
-            "/dev/block/by-name",
-            true,
-            true
-        )
-    }
-
 }
