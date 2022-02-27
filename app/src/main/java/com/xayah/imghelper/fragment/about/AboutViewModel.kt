73434eda5c30fa36ff20d963792d8ec43b9fb8b0
Xayah <zds1249475336@gmail.com>
2022-02-27 23:50:19 +0800

    feat: 添加关于页面内容

--- app/src/main/java/com/xayah/imghelper/fragment/about/AboutViewModel.kt
+++ app/src/main/java/com/xayah/imghelper/fragment/about/AboutViewModel.kt
@@ -1,5 +1,26 @@
 package com.xayah.imghelper.fragment.about
 
+import android.content.Intent
+import android.net.Uri
+import android.view.View
 import androidx.lifecycle.ViewModel
 
-class AboutViewModel : ViewModel() {}
+class AboutViewModel : ViewModel() {
+    fun toAppGitHub(v: View) {
+        val uri = Uri.parse("https://github.com/XayahSuSuSu/Android-IMGHelper")
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        v.context.startActivity(intent)
+    }
+
+    fun toAppAuthorCoolapk(v: View) {
+        val uri = Uri.parse("http://www.coolapk.com/u/1394294")
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        v.context.startActivity(intent)
+    }
+
+    fun toClashGitHub(v: View) {
+        val uri = Uri.parse("https://github.com/Kr328/ClashForAndroid")
+        val intent = Intent(Intent.ACTION_VIEW, uri)
+        v.context.startActivity(intent)
+    }
+}
